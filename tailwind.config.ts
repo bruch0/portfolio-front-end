4146e57971aac1bd76f86c9c73a9f157f8c23058
Lucas Bruch <lucas.dev@hublocal.com.br>
2025-02-26 10:36:36 -0300

    feat: create typography component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,11 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
 	"./stories/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist:[
+    {
+      pattern: /^(.*?)/,
+    },
+  ],
   theme: {
   	extend: {
   		colors: {
